ac59b900a743527f4f9deead3e79b9d492a9fa0f
Gobind <gobindsingh2004@gmail.com>
2022-08-09 22:38:56 -0700

    Fixing bugs

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+function menuOpen() {
+    document.querySelector('nav').classList += ' menu__open';
+}
 
-function getSearch(event) {
+function menuClose() {
+    document.querySelector('nav').classList.remove('menu__open');
+}
+
+function getSearch(event,buttonClick) {
     if (event.keyCode === 13) {
         localStorage.setItem("query",event.target.value)
-        window.location.href = `${window.location.origin}/search.html`
+    window.location.href = `${window.location.origin}/search.html`
+
+    }
+    else if (buttonClick) {
+        console.log(event.target.value);
+
     }
 }
+
+
--- search.js
+++ search.js
@@ -1,59 +1,69 @@
-let query = localStorage.getItem("query");
+
 let apiKey = "94c91bea";
 const searchResults = document.querySelector(".search__results");
 let data = [];
 
 function getSearch(event) {
   if (event.keyCode === 13) {
-      query = event.target.value;
-      renderResults();
+    localStorage.setItem("query",event.target.value) ;
+    renderResults();
   }
 }
 
 async function renderResults() {
-  document.querySelector(
-    ".displaysearchresults"
-  ).innerHTML = `Search Results for: "${query}"`;
-
-  const results = await fetch(
-    `http://www.omdbapi.com/?s=${query.replace(" ", "+")}&apikey=${apiKey}`
-  );
-  const resultsData = (await results.json()).Search;
-  console.log(resultsData); 
-  let promiseArr = [];
-
-  resultsData.forEach(result => {
-    promiseArr.push(fetch(`http://www.omdbapi.com/?i=${result.imdbID}&apikey=${apiKey}`).then((res) => res.json()))
-  });
-
-
-
-  await Promise.all(promiseArr).then((promise) => data = promise);
-
-  console.log(data);
-
-  if (resultsData) {
-    searchResults.innerHTML = data
-      .map((result) => resultsHTML(result))
-      .join("");
-  } else {
-    searchResults.innerHTML = `
-    Not Found. Please Try Again!
-    `;
+  let query = localStorage.getItem("query");
+  console.log(query);
+  if (localStorage.getItem("query")) {
+    window.localStorage.removeItem("query");
+    
+    document.querySelector(
+      ".displaysearchresults"
+    ).innerHTML = `Search Results for: "${query}"`;
+  
+    const results = await fetch(
+      `http://www.omdbapi.com/?s=${query.replace(" ", "+")}&apikey=${apiKey}`
+    );
+    const resultsData = (await results.json()).Search;
+    console.log(resultsData);
+    let promiseArr = [];
+    if (resultsData) {
+      resultsData.forEach((result) => {
+        promiseArr.push(
+          fetch(
+            `http://www.omdbapi.com/?i=${result.imdbID}&apikey=${apiKey}`
+          ).then((res) => res.json())
+        );
+      });
+  
+      await Promise.all(promiseArr).then((promise) => (data = promise));
+  
+  
+      searchResults.innerHTML = data
+        .map((result) => resultsHTML(result))
+        .join("");
+    } else {
+      searchResults.innerHTML = `
+      <div class="no__search__query">
+                <h2>Not Found. Please try again!</h2>
+                <figure class="no__search__query--wrapper"><img src="./assets/not-found.svg" alt=""></figure>
+              </div>
+      `;
+    }
   }
+  
+  
 }
-
 
 renderResults();
 
-
-
 function resultsHTML(result) {
-  return ` <div class="search__result">
+  return `<div class="search__result">
     <figure class="search__result--img--wrapper">
       <img
         src="${
-          result.Poster === "N/A" ? "./assets/no-img-available.webp" : result.Poster
+          result.Poster === "N/A"
+            ? "./assets/no-img-available.webp"
+            : result.Poster
         }"
         alt=""
         class="search__result--img"
@@ -62,7 +72,7 @@ function resultsHTML(result) {
       <div class="search__result--background--info">
         <p class="search__result--year">${result.Year}</p>
         <p class="search__result--description">
-          "${result.Plot}</h3>
+          "${result.Plot}"</h3>
         </p>
         <p class="search__result--genre">
         ${result.Genre}
@@ -71,25 +81,39 @@ function resultsHTML(result) {
     </figure>
     <div class="search__result--title">
       <h3 class="search__result--name">${result.Title}</h3>
-      <h4 class="search__result--type">(${result.Type[0].toUpperCase() + result.Type.substring(1)})</h4>
+      <div class="search__result--ratings">
+      ${ratingsHTML(result.imdbRating)}
+      </div>
+      <p class="search__result--description--low">
+        "${result.Plot}"</h3>
+        </p>
+      <h4 class="search__result--type">${
+        result.Type[0].toUpperCase() + result.Type.substring(1)
+      }</h4>
     </div>
-    <div class="search__result--ratings">
-    ${ratingsHTML(result.imdbRating)}
-    </div>
+   
   </div>`;
 }
 
 function ratingsHTML(rating) {
-  let ratingsHTML = '';
+  let ratingsHTML = "";
   rating /= 2;
-if (rating) {
-  for (let i = 0; i < Math.floor(rating); i++) {
-  ratingsHTML += `<i class="fa-solid fa-star"></i>`
+  if (rating) {
+    for (let i = 0; i < Math.floor(rating); i++) {
+      ratingsHTML += `<i class="fa-solid fa-star"></i>`;
+    }
+    if (!Number.isInteger(rating)) {
+      ratingsHTML += `<i class="fa-solid fa-star-half-stroke"></i>`;
+    }
   }
-  if (!Number.isInteger(rating)) {
-    ratingsHTML += `<i class="fa-solid fa-star-half-stroke"></i>`
-  }
+  return ratingsHTML;
 }
-return ratingsHTML;
+
+function menuOpen() {
+  document.querySelector('nav').classList += ' menu__open';
+}
+
+function menuClose() {
+  document.querySelector('nav').classList.remove('menu__open');
 }
 
